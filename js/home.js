30e3a849f1e2b2f3e0d75dbba4351683e7b1dfa7
William Seiti Mizuta <login+williammizuta@elo7.com>
2016-05-31 14:31:21 -0300

    Ajustes de modal

--- js/home.js
+++ js/home.js
@@ -11,4 +11,29 @@
 	mapa.addEventListener('mouseleave', function() {
 		mapa.classList.remove('selecionado');
 	});
+
+	window.addEventListener('hashchange', function() {
+		if(window.location.hash.match('#dialogo')) {
+			document.body.classList.add('dialogo-aberto');
+		} else {
+			document.body.classList.remove('dialogo-aberto');
+		}
+	});
+
+	document.addEventListener('keyup', function(e) {
+		if(e.keyCode == 27) {
+			window.location.hash = '#inicio';
+		}
+	});
+
+	var dialogos = document.getElementsByClassName('dialogo');
+	for(var i = dialogos.length - 1; i >= 0; i--) {
+		dialogos[i].addEventListener('click', function(e) {
+			e.stopPropagation();
+		});
+	}
+
+	document.body.addEventListener('click', function() {
+		window.location.hash = '#inicio';
+	});
 })();
